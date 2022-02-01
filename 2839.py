737fb86ee746eee258a0b075453d120c24bb66fe
soopeach <hsjeon584@gmail.com>
2022-02-01 23:58:31 +0900

    Not Yet

--- 2839.py
+++ 2839.py
@@ -1,15 +1,19 @@
 n = int(input())
 
 d = [0] * 5001
-
-for i in range(3,n+1) :
-
-    d[i] = d[i-1] + 1
+d[3] = 1
+for i in range(4,n+1) :
 
     if i % 3 == 0 :
-        d[i] = min(d[i//3]+1, d[i])
+        d[i] = (d[i//3] + 1)
     if i % 5 == 0 :
-        d[i] == min(d[i//5]+1, d[i])
+        d[i] = d[i//5] + 1
+
+
+    if i % 5 == 0 and i % 3 == 0 :
+        d[i] = min(d[i//5]+1, d[i//3]+1)
+    if i % 3 != 0 and i % 5 != 0 :
+        d[i] = -1
 
 
 print(d[n])
