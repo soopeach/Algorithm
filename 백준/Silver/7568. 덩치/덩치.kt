0bc58587eb9d79cad8927b082e5cf1efdf069ae2
Hyun_Soo <90144041+soopeach@users.noreply.github.com>
2022-04-21 20:26:17 +0900

    Update 덩치.kt

--- 백준/Silver/7568. 덩치/덩치.kt
+++ 백준/Silver/7568. 덩치/덩치.kt
@@ -1,21 +1,25 @@
-data class Size(var x : Int, var y : Int)
+// 몸무게, 키 / 한 쌍의 값을 담을 데이터 클래스
+data class Size(var weight : Int, var height : Int)
 
 fun main(){
 
+    // 데이터 입력을 받을 횟수(cnt)
     var cnt = readlnOrNull()!!.toInt()
+    // Size 데이터타입의 mutableList
     var sizeList = mutableListOf<Size>()
 
-
+    // cnt 횟수만큼 반복해서 키, 몸무게 데이터를 입력받음.
     for(no in 1 .. cnt){
         var input = readlnOrNull()!!.split(" ")
         var size = Size(input[0].toInt(), input[1].toInt())
         sizeList.add(size)
     }
 
+    // 각 데이터를 비교하여 순위를 매김.
     sizeList.forEach{
         cnt = 1
         sizeList.forEach { compared ->
-            if (it.x < compared.x && it.y < compared.y){
+            if (it.weight < compared.weight && it.height < compared.height){
                 cnt++
             }
         }
