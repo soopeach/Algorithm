7790bd295d0e39b159f048c678efad12d58ae594
Hyun_Soo <90144041+soopeach@users.noreply.github.com>
2022-04-27 23:30:11 +0900

    Update 더하기 사이클.kt

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.kt
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.kt
@@ -1,16 +1,22 @@
 fun main() {
-
+    
+    // 첫째 줄에 input을 입력 받음(N)
     var input = readln().toInt()
-    var tens = input / 10 
-    var units = input % 10 
+    // 십의 자리
+    var tens = input / 10
+    // 일의 자리
+    var units = input % 10
+    // 사이클 횟수
     var cnt = 1
-    var num = tens + units 
+    // 새로운 수
+    var num = tens + units
 
+    // input이 0이 아니라면 반복 - 입력값이 0 이라면 반복문을 실행하지 않고 cnt값인 1을 출력
     while (input != 0) {
-        tens = units 
-        units = num % 10 
-        num = tens + units 
-
+        tens = units
+        units = num % 10
+        num = tens + units
+        // 주어진 수가 새로운 수와 동일하다면
         if (input == (tens.toString() + units.toString()).toInt()) break
         cnt++
     }
