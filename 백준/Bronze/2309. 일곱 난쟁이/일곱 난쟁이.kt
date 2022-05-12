fd5dfb70eeade3b29ab0efa422f8f0c11250c4d3
Hyun_Soo <90144041+soopeach@users.noreply.github.com>
2022-05-12 18:13:36 +0900

    Update 일곱 난쟁이.kt

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.kt
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.kt
@@ -1,14 +1,13 @@
 fun main() {
 
     var nineDwarf = mutableListOf<Int>()
-    var originDwarf = mutableListOf<Int>()
 
     repeat(9) {
         nineDwarf.add(readln().toInt())
     }
 
+    var sum = nineDwarf.sum()
     for (fir in 0..8) {
-        var sum = nineDwarf.sum()
         for (sec in fir + 1..8) {
             if (sum - (nineDwarf[fir] + nineDwarf[sec]) == 100) {
                 nineDwarf.removeAt(sec)
@@ -20,5 +19,4 @@ fun main() {
             }
         }
     }
-
 }
