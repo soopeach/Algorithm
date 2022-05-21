0b3fcbf43b56f68ed14af492b85aae52a98daa8b
Hyun_Soo <90144041+soopeach@users.noreply.github.com>
2022-05-21 16:15:40 +0900

    [Silver III] Title: ATM, Time: 164 ms, Memory: 22468 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.kt
+++ 백준/Silver/11399. ATM/ATM.kt
@@ -1,9 +1,25 @@
+// 버블정렬 사용
 fun main() {
     var ans = 0
 
-    val size = readln().toInt()
-    val timeList = readln().split(" ").map { it.toInt() }.sorted().toIntArray()
+    readln().toInt()
+    val timeList = readln().split(" ").map { it.toInt() }.toIntArray()
 
+    // implement bubble sort
+    for (i in 0 until timeList.size){
+        var min = timeList[i]
+        var minIndex = i
+        for (j in i until timeList.size){
+            if (timeList[j] < min){
+                min = timeList[j]
+                minIndex = j
+            }
+        }
+        val temp = timeList[i]
+        timeList[i] = min
+        timeList[minIndex] = temp
+    }
+    
     for (index in 0 until timeList.size) {
         for (time in 0..index) {
             ans += timeList[time]
@@ -13,3 +29,20 @@ fun main() {
     println(ans)
 
 }
+
+// sorted() 사용
+//fun main() {
+//    var ans = 0
+//
+//    readln().toInt()
+//    val timeList = readln().split(" ").map { it.toInt() }.sorted().toIntArray()
+//
+//    for (index in 0 until timeList.size) {
+//        for (time in 0..index) {
+//            ans += timeList[time]
+//        }
+//    }
+//
+//    println(ans)
+//
+//}
