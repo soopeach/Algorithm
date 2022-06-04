bc368d06beb9686ff50159257ed63d0b0a50b9c5
Hyun_Soo <90144041+soopeach@users.noreply.github.com>
2022-06-04 17:40:35 +0900

    [Silver III] Title: 빈도 정렬, Time: 204 ms, Memory: 27636 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.kt
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.kt
@@ -1,22 +1,24 @@
 fun main() {
 
     val frequencySort = mutableMapOf<Int, Int>()
+    
     // 계수 정렬을 사용하기에는 C의 범위가 1,000,000,000 이하라서 무리...
     val (N, C) = readln().split(" ").map { it.toInt() }
 
     val sequence = readln().split(" ").map { it.toInt() }
 
     sequence.forEach {
-        if (frequencySort.containsKey(it)) {
-            frequencySort[it] = frequencySort[it]!! + 1
-        } else frequencySort.put(it, 1)
+
+        frequencySort.put(it, frequencySort.getOrDefault(it, 0) + 1)
     }
+
 
     frequencySort.toList().sortedByDescending { it.second }.toMap()
         .forEach { map ->
+            var ans =""
             repeat(map.value) {
-                print("${map.key} ")
+                ans += "${map.key} "
             }
+            print(ans)
         }
-
 }
