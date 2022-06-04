6cc5ce4a16420ae47e9f646c278484fb6e8e45d9
Hyun_Soo <90144041+soopeach@users.noreply.github.com>
2022-06-04 17:44:50 +0900

    [Silver III] Title: 파일 정리, Time: 1736 ms, Memory: 127860 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.kt
+++ 백준/Silver/20291. 파일 정리/파일 정리.kt
@@ -2,14 +2,10 @@ fun main(){
 
     val extentionList = mutableMapOf<String, Int>()
     repeat(readln().toInt()){
-        
+
         val extension = readln().split(".")[1]
         
-        if (extentionList.containsKey(extension)){
-            extentionList[extension] = extentionList[extension]!! + 1
-        } else {
-            extentionList.put(extension, 1)
-        }
+        extentionList.put(extension, extentionList.getOrDefault(extension, 0) + 1)
     }
 
     extentionList.toSortedMap().forEach{
