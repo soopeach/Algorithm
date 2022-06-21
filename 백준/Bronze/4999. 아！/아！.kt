df53803e6ca287d35e9dbac10c4a5ed95926e2a5
Hyun_Soo <90144041+soopeach@users.noreply.github.com>
2022-06-21 11:32:02 +0900

    [Bronze V] Title: 아!, Time: 96 ms, Memory: 12320 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    val jaeAhh = readln()
+    val docAhh = readln()
+
+    if (jaeAhh.length >= docAhh.length) {
+        println("go")
+    } else {
+        println("no")
+    }
+}
