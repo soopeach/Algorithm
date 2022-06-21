f93bd773f81abb9e8b1fae7382a5ba4736535a47
Hyun_Soo <90144041+soopeach@users.noreply.github.com>
2022-06-21 20:22:17 +0900

    [Silver V] Title: 숫자 카드, Time: 3288 ms, Memory: 227452 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.kt
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.kt
@@ -4,11 +4,10 @@ fun main() {
     val cardSet = readln().split(" ").map { it.toInt() }.toMutableSet()
     val m = readln().toInt()
 
-    // 상근이가 가지고 있는 카드번호를 입력받고 순차 탐색
+    // 상근이가 가지고 있는 카드번호인지 확인할 번호들을 입력받고 순차 탐색
     readln().split(" ").map { it.toInt() }.forEach {
         if (!cardSet.add(it)) print("1 ")
         else print("0 ")
     }
-
-
+    
 }
