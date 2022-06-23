043f91f25b8dbf009de94409f3c26c406cbce670
Hyun_Soo <90144041+soopeach@users.noreply.github.com>
2022-06-23 14:41:07 +0900

    Update 팀 나누기.kt

--- 백준/Bronze/13866. 팀 나누기/팀 나누기.kt
+++ 백준/Bronze/13866. 팀 나누기/팀 나누기.kt
@@ -1,9 +1,34 @@
 import kotlin.math.abs
 
+// 굳이 굳이 dfs 사용
+var minOfDiff = Int.MAX_VALUE
 fun main() {
 
     val players = readln().split(" ").map { it.toInt() }
-    val ans = abs(players.sum() - 2 * (players.minOf { it } + players.maxOf { it }))
-    println(ans)
-    
+    dfs(0,0,players,0)
+    println(minOfDiff)
+
 }
+
+fun dfs(cnt: Int, beginWith: Int, players: List<Int>, team: Int) {
+    val depth = 2
+    var team = team
+    if (cnt == depth){
+        val diffPower = abs(players.sum() - 2 * team)
+        if (minOfDiff > diffPower) minOfDiff = diffPower
+    }
+    for(index in beginWith until 4){
+        team += players[index]
+        dfs(cnt+1,index+1, players, team)
+        team -= players[index]
+    }
+}
+
+// 쉽게 쉽게
+// fun main() {
+//
+//    val players = readln().split(" ").map { it.toInt() }
+//    val ans = abs(players.sum() - 2 * (players.minOf { it } + players.maxOf { it }))
+//    println(ans)
+//
+//}
