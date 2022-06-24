1086a01cdebe0cf06704a6d78028b2d0c8078550
Hyun_Soo <90144041+soopeach@users.noreply.github.com>
2022-06-24 12:43:53 +0900

    [Silver I] Title: 신입 사원, Time: 2536 ms, Memory: 306240 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.kt
+++ 백준/Silver/1946. 신입 사원/신입 사원.kt
@@ -11,17 +11,20 @@ fun main(){
         var ans = 0
         // 현재까지 뽑힌 지원자의 최대 면접 순위
         var maxRank = Int.MAX_VALUE
+
         // 지원자의 성적순위를 저장
         repeat(n){
             val scoreData = readln().split(" ").map { it.toInt() }
             gradeList.add(Pair(scoreData[0],scoreData[1]))
         }
 
+        // 지원자의 서류심사 순위로 오름차순 정렬
         gradeList.sortBy{it.first}
 
         gradeList.forEachIndexed { index, curApplicant ->
 
-
+            // 지원자들의 서류심사 순위로 이미 정렬이 되었기 때문에 
+            // 현재 뽑힌 지원자들의 면접 순위의 가장 낮은 등수 보다 작다면 뽑힐 수 있다는 뜻
             if (maxRank > curApplicant.second){
                 maxRank = curApplicant.second
                 ans++
