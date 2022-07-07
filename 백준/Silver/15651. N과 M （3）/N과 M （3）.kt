1470c02d011e9945604f31eb02cbe49fd22da91f
Hyun_Soo <90144041+soopeach@users.noreply.github.com>
2022-07-07 15:05:57 +0900

    [Silver III] Title: N과 M (3), Time: 1048 ms, Memory: 300236 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.kt
@@ -0,0 +1,35 @@
+import java.io.BufferedWriter
+import java.io.OutputStreamWriter
+
+var numList = intArrayOf()
+val pickedNumList = mutableListOf<Int>()
+val ansList = mutableListOf<MutableList<Int>>()
+val bw = BufferedWriter(OutputStreamWriter(System.out))
+fun main() {
+
+    val (n, m) = readln().split(" ").map { it.toInt() }
+
+    for (no in 0..n) {
+        numList += no
+    }
+
+    dfs(0, m)
+    bw.flush()
+    bw.close()
+
+}
+
+fun dfs(cnt: Int, depth: Int) {
+    if (cnt == depth) {
+        bw.write("${pickedNumList.joinToString(" ")}\n")
+        return
+    }
+    for (index in 1 until numList.size) {
+        pickedNumList.add(numList[index])
+        dfs(cnt + 1, depth)
+        pickedNumList.removeAt(pickedNumList.lastIndex)
+    }
+}
+
+
+
