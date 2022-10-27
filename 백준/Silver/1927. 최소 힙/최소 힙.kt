93a177266f633345a63a0cd4890c70d040efda2c
soopeach <90144041+soopeach@users.noreply.github.com>
2022-10-28 00:50:39 +0900

    [Silver II] Title: 최소 힙, Time: 1352 ms, Memory: 33904 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.kt
+++ 백준/Silver/1927. 최소 힙/최소 힙.kt
@@ -1,20 +1,38 @@
-import java.util.PriorityQueue
+package hyunsoo.`7week`
 
+import java.util.*
+
+
+/**
+ * <문제>
+ * [최소 힙](https://www.acmicpc.net/problem/1927)
+ *
+ * Priority Queue를 사용하는 간단한 문제?
+ *
+ * 입/출력
+ * - 첫째 줄에 연산의 개수가 주어짐.
+ * - N개의 줄에 연산에 대한 정보를 나타내는 정수 x가 주어짐
+ *   - x가 자연수면 배열에 x를 추가, x가 0이면 가장 작은 값을 출력
+ */
 fun main() {
 
-    val heap = PriorityQueue<Int>()
+    val priorityQueue = PriorityQueue<Int>()
 
-    repeat(readln().toInt()) {
+    val cnt = readln().toInt()
 
-        val num = readln().toInt()
+    repeat(cnt) {
+        val command = readln().toInt()
 
-        if (num == 0) {
-            if (heap.isNotEmpty()) println(heap.poll())
-            else println(0)
-        } else {
-            heap.add(num)
+        when (command) {
+            0 -> {
+                priorityQueue.poll()?.let {
+                    println(it)
+                } ?: println(0)
+            }
+
+            else -> {
+                priorityQueue.add(command)
+            }
         }
     }
 }
-
-
