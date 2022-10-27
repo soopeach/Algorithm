e000b4161352937de9d00200250adca0060a5f31
soopeach <90144041+soopeach@users.noreply.github.com>
2022-10-28 01:03:46 +0900

    [Silver II] Title: 차이를 최대로, Time: 204 ms, Memory: 22952 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.kt
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.kt
@@ -7,31 +7,29 @@ val result = mutableListOf<Int>()
 var ans = 0
 fun main() {
 
-    permutation(0, size)
+    permutation(0)
     println(ans)
 
 }
 
-// 순열로 모든 경우의 수 확인
-fun permutation(cnt : Int, depth : Int) {
-    if (cnt == depth) {
-        // 이전 순열의 최대값(ans)보다 현재 순열의 최대값이 더 크다면 갱신
-        if (getMax(result) > ans) ans = getMax(result)
+fun permutation(cnt: Int) {
+    if (cnt == size) {
+        if (forAns(result) > ans) ans = forAns(result)
         return
     }
     for (i in 0 until num.size) {
         if (visited[i] == false) {
             visited[i] = true
             result.add(num[i])
-            permutation(cnt + 1, depth)
+            permutation(cnt + 1)
             result.removeAt(result.lastIndex)
             visited[i] = false
 
         }
     }
 }
-// 수식을 계산
-fun getMax(array: MutableList<Int>): Int {
+
+fun forAns(array: MutableList<Int>): Int {
     var ans = 0
     for (index in 2..array.size) {
         ans += abs(array[index - 2] - array[index - 1])
