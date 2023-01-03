9fdd59c4faad59b742114ec25d0835575d8114c2
soopeach <90144041+soopeach@users.noreply.github.com>
2023-01-04 05:20:41 +0900

    [D1] Title: 아주 간단한 계산기, Time: 6 ms, Memory: 344 KB -BaekjoonHub

---
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+
+    return 0;
+}
