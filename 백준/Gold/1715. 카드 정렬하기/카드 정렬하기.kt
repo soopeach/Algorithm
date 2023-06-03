56ab6a3d8ede67de82de8659e81ff8e6edfc55b9
soopeach <90144041+soopeach@users.noreply.github.com>
2023-06-04 00:16:01 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 552 ms, Memory: 26524 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.kt
@@ -0,0 +1,50 @@
+package hyunsoo.`35week`
+
+import java.util.PriorityQueue
+
+/**
+ *
+ * <문제>
+ * [카드 정렬하기](https://www.acmicpc.net/problem/1715)
+ *
+ * - 아이디어
+ *
+ * - 트러블 슈팅
+ *
+ */
+class `전현수_카드_정렬하기` {
+
+    fun solution() {
+
+
+        val priorityQueue = PriorityQueue<Int>()
+
+        var previousSum = 0
+        var answer = 0
+
+        repeat(readln().toInt()) {
+            priorityQueue.add(readln().toInt())
+        }
+
+        while (priorityQueue.size >= 2) {
+
+            val (previous, current) = priorityQueue.pollTwice()
+
+            val sum = previous + current
+            answer += sum
+
+            priorityQueue.add(sum)
+        }
+
+        println(answer)
+
+    }
+
+    private fun <T> PriorityQueue<T>.pollTwice(): Pair<T, T> {
+        return Pair(this.poll(), this.poll())
+    }
+}
+
+fun main() {
+    전현수_카드_정렬하기().solution()
+}
