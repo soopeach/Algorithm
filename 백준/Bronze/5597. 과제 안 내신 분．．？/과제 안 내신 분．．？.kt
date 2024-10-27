555e16f69a57b72cb1f70d7574c18f8d3613d442
전현수 <90144041+soopeach@users.noreply.github.com>
2024-10-27 18:15:31 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 80 ms, Memory: 12332 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.kt
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.kt
@@ -1,13 +1,47 @@
+/**
+ *
+ * <문제>
+ * []()
+ *
+ * - 아이디어
+ *
+ * - 트러블 슈팅
+ *
+ */
+class `전현수_과제_안_내신_분` {
+
+    val list = BooleanArray(31)
+
+    fun solution() {
+
+        repeat(28) {
+
+            val num = readln().toInt()
+
+            list[num] = true
+        }
+
+        var firstEmpty = -1
+
+        for (i in 1..30) {
+            if (list[i].not()) {
+                println(i)
+                firstEmpty = i
+                break
+            }
+        }
+
+        for (i in firstEmpty + 1..30) {
+            if (list[i].not()) {
+                println(i)
+                break
+            }
+        }
+
+
+    }
+}
+
 fun main() {
-
-    val array = BooleanArray(31, { false })
-
-    repeat(28) {
-        array[readln().toInt()] = true
-    }
-
-    array.forEachIndexed { index, it ->
-
-        if (!it && index != 0) println(index)
-    }
+    전현수_과제_안_내신_분().solution()
 }
